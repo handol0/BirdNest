5ac762e0d73d8b954cad7d8dca2c2add5709130b
handol0 <85655626+handol0@users.noreply.github.com>
2022-05-22 11:14:20 +0900

    Helping Charlie

---
+++ Birds/Python/NewCharlie.py
@@ -0,0 +1,22 @@
+import pandas_datareader
+import yfinance as yf
+import pandas as pd
+from pandas_datareader import data as pdr
+
+writer = pd.ExcelWriter('E:\Folder\data\charlie.xlsx')
+
+Ticker =  ['BA', 'TSLA', 'NKE', '^DJI']
+
+dataBTND = pdr.get_data_yahoo(Ticker, start="2012-01-01", end="2021-12-31", interval='m')
+dataBTND = dataBTND['Close']
+
+dataBTND.to_excel(writer, sheet_name='Sheet1', header=True)
+
+#================================================================================
+
+Ticker2 = ['GC=F','^IRX']
+dataGI = pdr.get_data_yahoo(Ticker2, start="2012-01-01", end="2021-12-31", interval='m')
+dataGI = dataGI['Close']
+
+dataGI.to_excel(writer, sheet_name='Sheet2', header=True)
+writer.save()
--- Birds/Python/YFinaceCheck.py
+++ Birds/Python/YFinaceCheck.py
@@ -1,6 +1,33 @@
-import yfinance
-import pandas_datareader as pd
+import pandas_datareader
+import yfinance as yf
+import pandas as pd
+from pandas_datareader import data as pdr
+
+writer = pd.ExcelWriter('E:\Folder\data\charlie.xlsx')
+
+
+dataBA = pdr.get_data_yahoo("BA", start="2012-01-01", end="2021-12-31", interval='m')
+dataBA = dataBA['Close']
+#dataBA['CName'] = "The Boeing Company"
+
+dataTesla = pdr.get_data_yahoo("TSLA", start="2012-01-01", end="2021-12-31", interval='m')
+dataTesla = dataTesla['Close']
+#dataTesla['CName'] = "TESLA"
+
+dataNike = pdr.get_data_yahoo("NKE", start="2012-01-01", end="2021-12-31", interval='m')
+dataNike = dataNike['Close']
+#dataNike['CName'] = "Nike, Inc"
+
+dataDJ = pdr.get_data_yahoo("^DJI", start="2012-01-01", end="2021-12-31", interval='m')
+dataDJ = dataDJ['Close']
+#dataDJ['CName'] = "Dow Jones"
+print(dataBA)
+
+
+#union진행 필요
+union_all=pd.concat([dataBA, dataTesla, dataNike, dataDJ])
 
 
 
-
+union_all.to_excel(writer, sheet_name='KOSPI', header=True)
+writer.save()
