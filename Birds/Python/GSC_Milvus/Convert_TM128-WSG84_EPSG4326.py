d4a6613ace02dac79af585299634bc5765b9ed1d
handol0 <handol0@hotmail.com>
2022-05-22 22:21:52 +0900

    Minor changes

--- Birds/Python/GSC_Milvus/Convert_TM128-WSG84_EPSG4326.py
+++ Birds/Python/GSC_Milvus/Convert_TM128-WSG84_EPSG4326.py
@@ -1,6 +1,6 @@
 #Code :  https://domdom.tistory.com/entry/pandas-%EC%A2%8C%ED%91%9C%EA%B3%84-xy%ED%96%89-%EB%AA%A8%EB%91%90-%EB%8B%A4%EB%A5%B8-%EC%A2%8C%ED%91%9C%EA%B3%84%EB%A1%9C-%EB%B3%80%EA%B2%BD%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
 #Tableau Description :  http://www.datavisualization.co.kr/%ED%83%9C%EB%B8%94%EB%A1%9C-utm-k-%EC%A2%8C%ED%91%9C-%EA%B0%92%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%B4-%EC%A7%80%EB%8F%84%EB%A1%9C-%ED%91%9C%ED%98%84%ED%95%98%EA%B8%B0/
-import pyproj
+
 from pyproj import Proj, transform
  
  
