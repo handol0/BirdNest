a474a63345045d3cd4c505fc6438049a2c81c1fb
handol0 <handol0@hotmail.com>
2022-05-22 21:44:47 +0900

    Folder Added + GSC Project

---
+++ Birds/Python/GSC_Milvus/Convert_TM128-WGS84(EPSG4326).py
@@ -0,0 +1,8 @@
+#https://domdom.tistory.com/entry/pandas-%EC%A2%8C%ED%91%9C%EA%B3%84-xy%ED%96%89-%EB%AA%A8%EB%91%90-%EB%8B%A4%EB%A5%B8-%EC%A2%8C%ED%91%9C%EA%B3%84%EB%A1%9C-%EB%B3%80%EA%B2%BD%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
+
+
+
+
+
+
+
